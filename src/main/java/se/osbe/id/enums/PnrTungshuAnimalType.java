091461ef2d9b1e8c70dfa7c2241d79f4bdadb1b9
Oskar Bergström <rapidfish@me.com>
2024-04-18 14:57:39 +0200

    Refak, moved method method getTypeForYear from enum PnrTungshuAnimalType to static helper class PersonnummerHelper

--- src/main/java/se/osbe/id/enums/PnrTungshuAnimalType.java
+++ src/main/java/se/osbe/id/enums/PnrTungshuAnimalType.java
@@ -2,6 +2,7 @@ package se.osbe.id.enums;
 
 
 import org.joda.time.DateTime;
+import se.osbe.id.helper.PersonnummerHelper;
 
 import static java.util.Objects.requireNonNull;
 
@@ -49,44 +50,6 @@ public enum PnrTungshuAnimalType {
     }
 
     public static PnrTungshuAnimalType getTypeForYear(DateTime date) {
-        requireNonNull(date);
-        final int DIVIDER = 12;
-        final int YEAR_OF_THE_RAT = 2020; // the year of the rat is the first animal of a twelve-year cycle in the Tungshu Zodiac
-        int yearCandidate = date.getYear();
-        int distance = Math.abs(yearCandidate - YEAR_OF_THE_RAT);
-        int mod = distance % DIVIDER;
-        if (yearCandidate == YEAR_OF_THE_RAT || mod == 0) {
-            return RAT; // offset is destined to be 0, thus the result is the year of the rat.
-        }
-        boolean isBefore2020 = yearCandidate < YEAR_OF_THE_RAT;
-        int offset = isBefore2020 ? DIVIDER - mod : mod;
-        switch (offset) {
-            case 0:
-                return RAT;
-            case 1:
-                return OX;
-            case 2:
-                return TIGER;
-            case 3:
-                return RABBIT;
-            case 4:
-                return DRAGON;
-            case 5:
-                return SNAKE;
-            case 6:
-                return HORSE;
-            case 7:
-                return GOAT;
-            case 8:
-                return MONKEY;
-            case 9:
-                return ROOSTER;
-            case 10:
-                return DOG;
-            case 11:
-                return PIG;
-            default:
-                throw new IllegalArgumentException("No match for animal type and year: " + yearCandidate);
-        }
+        return PersonnummerHelper.getTypeForYear(date);
     }
 }
--- src/main/java/se/osbe/id/helper/PersonnummerHelper.java
+++ src/main/java/se/osbe/id/helper/PersonnummerHelper.java
@@ -1,9 +1,11 @@
 package se.osbe.id.helper;
 
 import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 import org.joda.time.format.DateTimeFormat;
 import se.osbe.id.enums.LocationType;
+import se.osbe.id.enums.PnrTungshuAnimalType;
 import se.osbe.id.enums.PnrZodiacType;
 import se.osbe.id.Personnummer;
 
@@ -15,6 +17,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.Objects.requireNonNull;
+import static se.osbe.id.enums.PnrTungshuAnimalType.*;
 
 public class PersonnummerHelper {
 
@@ -39,6 +44,48 @@ public class PersonnummerHelper {
             }
         }
         return Optional.empty();
+    }
+
+    public static PnrTungshuAnimalType getTypeForYear(DateTime date) {
+        requireNonNull(date);
+        final int DIVIDER = 12;
+        final int YEAR_OF_THE_RAT = 2020; // the year of the rat is the first animal of a twelve-year cycle in the Tungshu Zodiac
+        int yearCandidate = date.getYear();
+        int distance = Math.abs(yearCandidate - YEAR_OF_THE_RAT);
+        int mod = distance % DIVIDER;
+        if (yearCandidate == YEAR_OF_THE_RAT || mod == 0) {
+            return RAT; // offset is destined to be 0, thus the result is the year of the rat.
+        }
+        boolean isBefore2020 = yearCandidate < YEAR_OF_THE_RAT;
+        int offset = isBefore2020 ? DIVIDER - mod : mod;
+        switch (offset) {
+            case 0:
+                return RAT;
+            case 1:
+                return OX;
+            case 2:
+                return TIGER;
+            case 3:
+                return RABBIT;
+            case 4:
+                return DRAGON;
+            case 5:
+                return SNAKE;
+            case 6:
+                return HORSE;
+            case 7:
+                return GOAT;
+            case 8:
+                return MONKEY;
+            case 9:
+                return ROOSTER;
+            case 10:
+                return DOG;
+            case 11:
+                return PIG;
+            default:
+                throw new IllegalArgumentException("No match for animal type and year: " + yearCandidate);
+        }
     }
 
     /**
