59c9b0563a1fa84d81f52f210fddf1c98d66dc39
Oskar Bergström <rapidfish@me.com>
2024-05-07 13:13:04 +0200

    Refak of PersonnummerStatisticsHelper in an attempt to modernize it using lambda expressions

--- src/main/java/se/osbe/id/helper/PersonnummerStatisticsHelper.java
+++ src/main/java/se/osbe/id/helper/PersonnummerStatisticsHelper.java
@@ -8,11 +8,12 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PersonnummerStatisticsHelper {
     private Map<String, Personnummer> _pnrMap;
     private List<Personnummer> _pnrList;
-    private List<Personnummer> _pnrDuplicatesList;
+    private List<Personnummer> _pnrDuplicateStorageList;
     private int _amountMen;
     private int _amountWomen;
     private int _maxAge;
@@ -27,12 +28,12 @@ public class PersonnummerStatisticsHelper {
         if (pnrList == null || pnrList.isEmpty()) {
             throw new PersonnummerException("pnrList is null or empty");
         }
-        _pnrDuplicatesList = new LinkedList<Personnummer>();
+        _pnrDuplicateStorageList = new LinkedList<Personnummer>();
         _pnrMap = new HashMap<String, Personnummer>();
         for (Personnummer pnr : pnrList) {
             String pnrStr = pnr.toString();
             if (_pnrMap.put(pnrStr, pnr) != null) {
-                _pnrDuplicatesList.add(pnr);
+                _pnrDuplicateStorageList.add(pnr);
             }
         }
         _pnrList = new LinkedList<Personnummer>(_pnrMap.values());
@@ -44,48 +45,24 @@ public class PersonnummerStatisticsHelper {
     }
 
     private int resolveHighestAge() {
-        int age = _pnrList.get(0).getAgeNow();
-        int highestAge = 0;
-        for (Personnummer pnr : _pnrList) {
-            age = pnr.getAgeNow();
-            highestAge = (age > highestAge) ? age : highestAge;
-        }
-        return highestAge;
+        return _pnrList.stream().map(pnr -> pnr.getAgeNow()).max(Integer::compare).get();
     }
 
     private int resolveLowestAge() {
-        int age = _pnrList.get(0).getAgeNow();
-        int lowestAge = age;
-        for (Personnummer pnr : _pnrList) {
-            age = pnr.getAgeNow();
-            lowestAge = (age < lowestAge) ? age : lowestAge;
-        }
-        return lowestAge;
+        return _pnrList.stream().map(pnr -> pnr.getAgeNow()).min(Integer::compare).get();
     }
 
     private double resolveAverageAge() {
-        double average = 0.0;
-        double sum = 0.0;
-        for (Personnummer pnr : _pnrList) {
-            sum += pnr.getAgeNow();
-        }
-        average = sum / _pnrList.size();
-        return average;
+        return _pnrList.stream().mapToDouble(pnr -> pnr.getAgeNow()).average().getAsDouble();
     }
 
     private void resolveGenders() {
-        _amountMen = 0;
-        _amountWomen = 0;
-        for (Personnummer pnr : _pnrList) {
-            if (pnr.getGender().equals(GenderType.MALE))
-                _amountMen++;
-            else if (pnr.getGender().equals(GenderType.FEMALE))
-                _amountWomen++;
-        }
+        _amountMen = (int) _pnrList.stream().filter(pnr -> pnr.getGender().equals(GenderType.MALE)).count();
+        _amountWomen = (int) _pnrList.stream().filter(pnr -> pnr.getGender().equals(GenderType.FEMALE)).count();
     }
 
     public List<Personnummer> getPnrDuplicateList() {
-        return _pnrDuplicatesList;
+        return _pnrDuplicateStorageList;
     }
 
     public List<Personnummer> getAllPnr() {
@@ -104,6 +81,10 @@ public class PersonnummerStatisticsHelper {
         return allWithAge;
     }
 
+    public List<Personnummer> extractAllSamordningsnummer() {
+        return _pnrList.stream().filter(p -> p.isSamordningsnummer()).collect(Collectors.toList());
+    }
+
     public LinkedList<Personnummer> extractAllPnrWithAgeUnder(int age) {
         LinkedList<Personnummer> allUnderAge = new LinkedList<Personnummer>();
         if (!_pnrList.isEmpty()) {
@@ -116,30 +97,16 @@ public class PersonnummerStatisticsHelper {
         return allUnderAge;
     }
 
-    public LinkedList<Personnummer> extractAllPnrWithAgeOver(int age) {
-        LinkedList<Personnummer> allOverAge = new LinkedList<Personnummer>();
-        if (!_pnrList.isEmpty()) {
-            for (Personnummer pnr : _pnrList) {
-                if (pnr.getAgeNow() > age) {
-                    allOverAge.add(pnr);
-                }
-            }
-        }
-        return allOverAge;
+    public List<Personnummer> extractAllPnrWithAgeOver(int age) {
+        return _pnrList.stream().filter(p -> p.getAgeNow() > age).collect(Collectors.toList());
     }
 
-    public LinkedList<Personnummer> getAllPnrAverageAge(int diff) {
-        LinkedList<Personnummer> allAverage = new LinkedList<Personnummer>();
-        Double average = resolveAverageAge();
-        if (!_pnrList.isEmpty()) {
-            for (Personnummer pnr : _pnrList) {
-                double ageNow = pnr.getAgeNow();
-                if (ageNow >= (average - diff) && ageNow <= (average + diff)) {
-                    allAverage.add(pnr);
-                }
-            }
-        }
-        return allAverage;
+    public List<Personnummer> getAllPnrAverageAge(int diff) {
+        double average = resolveAverageAge();
+        return _pnrList.stream()
+                .filter(p -> ((double)p.getAgeNow()) >=  (average - diff))
+                .filter(p -> ((double)p.getAgeNow()) <=  (average + diff))
+                .collect(Collectors.toList());
     }
 
     public int getMaxAge() {
